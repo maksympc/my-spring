0f2153db197b0c4e0dc9664b16a2d2ffccd9290c
Maksym Pinchuk <maksympc@gmail.com>
2018-01-16 23:24:17 +0200

    add exampele with Robot's parts, simple ApplicationContext configuration, with property and constructor

---
+++ src/main/java/com/training/spring/impls/toshiba/ToshibaHand.java
@@ -0,0 +1,11 @@
+package com.training.spring.impls.toshiba;
+
+import com.training.spring.interfaces.Hand;
+
+public class ToshibaHand implements Hand {
+	
+	public void catchSomething(){
+		System.out.println("Catched from Toshiba!");
+	}
+
+}
