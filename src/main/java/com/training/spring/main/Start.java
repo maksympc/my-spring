27d18a8ad0f1f965a1cdc2ae2a50654ed4c71f9a
Maksym Pinchuk <maksympc@gmail.com>
2018-01-17 21:39:28 +0200

    add example with p and c xmlns use for simplifying parameter passing

--- src/main/java/com/training/spring/main/Start.java
+++ src/main/java/com/training/spring/main/Start.java
@@ -6,10 +6,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Start {
-	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
-		t1000.dance();
-		((ConfigurableApplicationContext)context).close();// закрытие контекста вручную
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
+        t1000.action();
+        ((ConfigurableApplicationContext) context).close();// закрытие контекста вручную
+    }
 }
